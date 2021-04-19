a5fd03a0f26e91a6e8c3e7abc15cacfe4290ae17
wwkyip <wai.yip@cultureamp.com>
2021-04-19 17:14:14 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+"""
+Created on Mon Apr 19 14:56:36 2021
+
+@author: winnyyip
+"""
+
+import pandas as pd
+import plotly_clab as pc
+
+# data preparation
+
+data = pd.DataFrame({'region':['Asia','North America','South America', 'Middle East', 'South East Asia', 'Europe', 'Africa'],
+                     'value':[10,40,50,60,30,25,46]})
+
+data2 = pd.DataFrame({'Quarter': ['2020-Q2','2020-Q3','2020-Q4','2021-Q1'],
+ 'Purpose':	[70,66,65,64],
+ 'Support':	[58,53,51,52],
+ 'Self':  	[50,47,46,48,],
+ 'Balance':  [41,42,41,41]})
+
+data3 = pd.read_excel('scores.xlsx')
+
+
+# bar plot
+bar_div  = pc.plot_bar_vertical(data, 'region', 'value', 'Engagement Score by Region')
+#bar_div2 = pc.plot_bar_horizontal(data, 'region', 'value', 'Engagement Score by Region')
+
+
+# line plot
+line_div = pc.plot_line(data2, 'Quarter', ['Purpose','Support','Self','Balance'], "text+name", "Wellbeing scores by Quarter")
+
+
+# scatter plot
+scatter_div = pc.plot_scatter(data3, 'eng_1', 'eng_5', 'size', 'bm_industry', 'Correlation of Eng 1 to Eng 5 by Industry')
+
+
+# prepare html to insert graphs
+plotlyjs = """
+<head>
+  <link rel="stylesheet" href="https://d1vmr11cgrgrrj.cloudfront.net/7834392/css/fonts.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+  <script>
+  function myFunction(elementid) {
+      var x = document.getElementById(elementid);
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+  }
+  </script>
+</head>"""  
+
+html = plotlyjs 
+html += "{}{}{}".format(bar_div, line_div, scatter_div) 
+
+n = open("graphs.html", "w")
+n.write(html)
+n.close()
---
+++ plotly_clab.py
@@ -0,0 +1,95 @@
+
+
+import numpy
+
+
+# plotly
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+color_bar =  ['#F04C5D',  # coral
+              '#45AD8F',  # seedling
+              '#253C64',  # lapis
+              '#FFCE1E',  #yuzu
+              '#727193',  # wistaria
+              '#ffc990', 
+              '#b0dfd2']  # just example here Michael please alter
+
+
+def plot_bar_vertical(table, cat_col, val_col, title):
+    
+    ''' Bar plot '''
+    
+    table = table.sort_values(val_col)
+    
+    fig = go.Figure(go.Bar(
+            x=table[val_col],
+            y=table[cat_col],
+            orientation = 'h',
+            marker_color = color_bar))
+    
+    fig.update_layout(
+      title = title,
+      showlegend = False
+    )
+      
+    div = plot(fig,auto_open=False,include_plotlyjs=False,output_type='div', config={'displayModeBar': False,'showLink':False})     
+    return div   
+    
+    
+    
+    
+def plot_line(table, x_col, y_col, hover_label, title):
+    
+    ''' Line plot '''
+    
+    fig = go.Figure()
+    
+    for c in y_col:
+        fig.add_trace(go.Scatter(x = table[x_col], 
+                                 y = table[c], 
+                                 name = c,
+                    line_shape='linear'))
+
+    fig.update_traces(hoverinfo = hover_label, mode='lines+markers')
+    fig.update_layout(title = title, legend=dict(y=0.5, traceorder='reversed', font_size=16))
+    
+
+    div = plot(fig,auto_open=False,include_plotlyjs=False,output_type='div', config={'displayModeBar': False,'showLink':False})     
+    return div      
+    
+    
+    
+    
+def plot_scatter(table, x_col, y_col, size_col, group_col, title):
+    
+    ''' Scatter plot '''
+    traces = []
+    
+    for g in table[group_col].unique():
+        
+        cond = table[group_col] == g
+        traces += [go.Scatter(
+                x = table.loc[cond, x_col],
+                y = table.loc[cond, y_col],
+                mode = 'markers',
+                name = g,
+                marker = dict(size = table.loc[cond, size_col]*6)
+                )]
+    
+    
+    layout = go.Layout(
+                    title = title,
+                    plot_bgcolor = 'rgb(229, 229, 229)',
+                    xaxis=go.XAxis(zerolinecolor='rgb(255,255,255)', gridcolor='rgb(255,255,255)'),
+                    yaxis=go.YAxis(zerolinecolor='rgb(255,255,255)', gridcolor='rgb(255,255,255)'),
+                    )
+    
+    fig = go.Figure(data=traces, layout=layout)    
+    
+
+    div = plot(fig,auto_open=False,include_plotlyjs=False,output_type='div', config={'displayModeBar': False,'showLink':False})     
+    return div   
+
+
+
